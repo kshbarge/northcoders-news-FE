8edaf7b412a760867127474ffa0a6fac3c883b48
kshbarge <kshbarge@posteo.net>
2025-06-27 16:25:56 +0100

    Added the ability to sort and order the article list and topic sections

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -6,23 +6,40 @@ import ArticleCard from "./ArticleCard";
 
 function ArticleList(){
     const [articlesData, setArticlesData] = useState([]);
+    const [selectedSort, setSelectedSort] = useState('created_at');
+    const [selectedOrder, setSelectedOrder] = useState('desc');
     const { topicName } = useParams()
 
     useEffect(() => {
-        let contentQuery = "articles"
+        let contentQuery = `articles?sort_by=${selectedSort}&&order=${selectedOrder}`
         if(topicName){
-          contentQuery += `?topic=${topicName}`
+          contentQuery += `&&topic=${topicName}`
         }
+        console.log(contentQuery)
         getContent(contentQuery).then(({articles}) => {
             setArticlesData(articles)
         })
-    }, [topicName])
+    }, [topicName, selectedSort, selectedOrder])
 
     return (
         <>
           <section>
             {topicName ? <h2>{topicName}</h2> : <h2>All articles</h2>}
-            <p>I'm a placeholder for the all articles/sort by bar</p>
+            <form method="get">
+              <label htmlFor="sortOptions">Sort by:</label>
+              <select name="sortOptions" id="sortOptions" value={selectedSort} onChange={e => setSelectedSort(e.target.value)}>
+                <option value="created_at">Created at</option>
+                <option value="votes">Votes</option>
+                <option value="topic">Topic</option>
+                <option value="title">Title</option>
+                <option value="author">Author</option>
+              </select>
+              <label htmlFor="orderOptions">Order by:</label>
+              <select name="orderOptions" id="orderOptions" value={selectedOrder} onChange={e => setSelectedOrder(e.target.value)}>
+                <option value="desc">Descending</option>
+                <option value="asc">Ascending</option>
+              </select>
+            </form>
           </section>
           <section>
             {articlesData.map((article) => {
