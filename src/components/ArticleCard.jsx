fd8966d0a94d9ffbc5265fb091b3b0c438b853e4
kshbarge <kshbarge@posteo.net>
2025-07-25 17:43:31 +0100

    remove commented out code

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -19,15 +19,6 @@ function ArticleCard ({article}){
               <p>{humanDate.toString()}</p>
             </div>
           </div>
-          {/*<div className="articleCardBox">
-              <img src={article_img_url} className="articleCardImage"/>
-              <h3>{title}</h3>
-              <p>{author}</p>
-              <p>{topic}</p>
-              <p>Votes: {votes}</p>
-              <p>Comments: {comment_count}</p>
-              <p>{humanDate.toString()}</p>
-          </div>*/}
         </Link>
     )
 }
