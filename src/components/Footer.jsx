7e299b6799410e730cb11088a99f6ce1bb05f448
kshbarge <kshbarge@posteo.net>
2025-08-04 22:33:11 +0100

    style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 function Footer (){
     return (
-      <>
-        <p>NC news created by Kathryn Barge, 2025</p>
-        <a target="_blank" href="https://icons8.com/icon/routpOrsm6F0/morning-news">Morning News</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-      </>
+      <div class="fixed-bottom text-bg-danger p-3">
+        <p class="text-center">NC News created by Kathryn Barge, 2025. <a target="_blank" href="https://icons8.com/icon/routpOrsm6F0/morning-news" class="link-light">Morning News</a> icon by <a target="_blank" href="https://icons8.com" class="link-light">Icons8</a>.</p>
+      </div>
     )
 }
 
