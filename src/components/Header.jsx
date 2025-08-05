1acaf57a19fbc62b9f5cab357a858285b60a84dc
kshbarge <kshbarge@posteo.net>
2025-08-05 18:48:13 +0100

    begin header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ import { UserContext } from '../contexts/UserContext'
 function Header (){
   const {loggedInUser} = useContext(UserContext)
     return (
-        <>
-          <h1>NC News</h1>
-          <nav>
-            <Link to="/">Home </Link>
-            <Link to="/articles">Articles </Link>
-            <Link to="/topics">Topics </Link>
+        <div class="container-fluid">
+          <h1 class="text-center">NC News</h1>
+          <nav class="navbar bg-danger p-3 mb-2">
+            <Link to="/" class="nav-link link-light">Home </Link>
+            <Link to="/articles" class="nav-link link-light">Articles </Link>
+            <Link to="/topics" class="nav-link link-light">Topics </Link>
+            <span class="navbar-text text-white d-flex">You are currently logged in as: {loggedInUser.username}</span>
           </nav>
-          <p>You are currently logged in as: {loggedInUser.username}</p>
-        </>
+        </div>
     )
 }
 
