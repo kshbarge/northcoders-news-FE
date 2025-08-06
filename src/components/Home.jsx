5984f6948b1416dd03700446dd7ba58afa508f1f
kshbarge <kshbarge@posteo.net>
2025-08-06 18:09:49 +0100

    centre elements on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom"
 
 function Home (){
     return (
-        <>
-          <h2>Welcome to NC News!</h2>
+        <div class="d-flex flex-column align-items-center">
+          <h2 class="text-center">Welcome to NC News!</h2>
           <p>NC news is a cross between your favourite news sites and social media. Browse through and read articles, vote on your faves, and comment to let others know what you think!</p>
           <Link to="/articles">
             <button type="button" class="btn btn-danger btn-lg">To articles</button>
           </Link>
-        </>
+        </div>
     )
 }
 
