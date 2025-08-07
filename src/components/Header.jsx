0348f6ebf50aabadc29566e21f017a546edda06e
kshbarge <kshbarge@posteo.net>
2025-08-07 23:57:12 +0100

    modify navbar spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,30 @@ import { UserContext } from '../contexts/UserContext'
 function Header (){
   const {loggedInUser} = useContext(UserContext)
     return (
-        <div class="container-fluid">
+        <>
           <h1 class="text-center">NC News</h1>
-          <nav class="navbar bg-danger p-3 mb-2">
-            <Link to="/" class="nav-link link-light">Home </Link>
-            <Link to="/articles" class="nav-link link-light">Articles </Link>
-            <Link to="/topics" class="nav-link link-light">Topics </Link>
-            <span class="navbar-text text-white d-flex">You are currently logged in as: {loggedInUser.username}</span>
+          <nav class="navbar navbar-expand-lg bg-danger p-3 mb-2">
+            <div class="container-fluid">
+              <Link to="/" class="navbar-brand nav-link link-light">
+                Home 
+              </Link>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <Link to="/articles" class="nav-link link-light">Articles </Link>
+                  </li>
+                  <li class="nav-item">
+                    <Link to="/topics" class="nav-link link-light">Topics </Link>
+                  </li>
+                </ul>
+                <span class="navbar-text text-white d-flex">You are currently logged in as: {loggedInUser.username}</span>
+              </div>
+            </div>
           </nav>
-        </div>
+        </>
     )
 }
 
