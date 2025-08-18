ec06da978b6245d8e3fe74cb2431317180fe437a
kshbarge <kshbarge@posteo.net>
2025-08-18 23:45:40 +0100

    add card layout to topics list

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -5,10 +5,14 @@ function TopicCard ({topic}){
 
     return (
         <Link to={`/topics/${slug}`}>
-            <div className="topicCardBox">
-                <img src={img_url ? img_url : "https://picsum.photos/id/2/250/300" } className="topicCardImage"/>
-                <h3>{slug}</h3>
-                <p>{description}</p>
+            <div class="col-sm">
+              <div class="card m-3">
+                  <img src={img_url ? img_url : "https://picsum.photos/id/2/250/165" } class="card-img-top" alt="..."/>
+                  <div class="card-body">
+                    <h3 class="card-title">{slug}</h3>
+                    <p>{description}</p>
+                  </div>
+              </div>
             </div>
         </Link>
     )
--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -16,11 +16,11 @@ function TopicsList (){
           <section>
             <h2>All topics</h2>
           </section>
-          <section>
+          <div class="row row-cols-sm-2 row-cols-md-3">
             {topicsData.map((topic) => {
                 return <TopicCard key={topic.slug} topic={topic}/>
             })}
-          </section>
+          </div>
         </>
     )
 }
