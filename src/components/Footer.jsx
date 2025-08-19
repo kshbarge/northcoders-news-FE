5433e901bda7f382b30edce9472b277f3db20b5c
kshbarge <kshbarge@posteo.net>
2025-08-19 23:54:16 +0100

    round footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer (){
     return (
-      <div class="text-bg-danger p-3">
+      <div class="text-bg-danger rounded-top-5 p-1">
         <p class="text-center">NC News created by Kathryn Barge, 2025. <a target="_blank" href="https://icons8.com/icon/routpOrsm6F0/morning-news" class="link-light">Morning News</a> icon by <a target="_blank" href="https://icons8.com" class="link-light">Icons8</a>.</p>
       </div>
     )
